38b78c381d71956f072f8682e0bb669900ee7a2e
Jonathan Henrique <jonathan.medeiros@picpay.com>
2022-03-01 16:55:58 -0300

    Adding UUID to categories resource

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,13 +2,19 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, Uuid;
 
+    public $incrementing = false;
+    protected $keyType = 'string';
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
+    protected $casts = [
+        'id' => 'string'
+    ];
 }
