ddb047d283b0386b0353f0fd9496d5c08a133051
Jonathan Henrique <jonathan.medeiros@picpay.com>
2022-03-01 16:16:42 -0300

    Adding PUT resource to categories

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    private $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean'
+    ];
+
     public function index()
     {
         return Category::all();
@@ -15,10 +20,7 @@ class CategoryController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required|max:255',
-            'is_active' => 'boolean'
-        ]);
+        $this->validate($request, $this->rules);
         return Category::create($request->all());
     }
 
@@ -29,7 +31,8 @@ class CategoryController extends Controller
 
     public function update(Request $request, Category $category)
     {
-        //
+        $this->validate($request, $this->rules);
+        return $category->update($request->all());
     }
 
     public function destroy(Category $category)
