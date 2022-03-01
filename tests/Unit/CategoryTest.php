cbd2732c23239ee66511751a912018d71380442d
Jonathan Henrique <jonathan.medeiros@picpay.com>
2022-03-01 20:37:20 -0300

    Setup test environment

--- tests/Unit/CategoryTest.php
+++ tests/Unit/CategoryTest.php
@@ -2,17 +2,51 @@
 
 namespace Tests\Unit;
 
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Category;
 use PHPUnit\Framework\TestCase;
 
 class CategoryTest extends TestCase
 {
-    public function testCategoryFillable()
+    public function testIfUsingTraits()
+    {
+        $expected = [
+            SoftDeletes::class,
+            Uuid::class
+        ];
+        $actual = array_keys(class_uses(Category::class));
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testFillableAttribute()
     {
         $category = new Category();
-        $this->assertEquals(
-            ['name', 'description', 'is_active'],
-            $category->getFillable()
-        );
+        $expected = ['name', 'description', 'is_active'];
+        $this->assertEquals($expected, $category->getFillable());
+    }
+
+    public function testCastsAttribute()
+    {
+        $category = new Category();
+        $expected = ['id' => 'string'];
+        $this->assertEquals($expected, $category->getCasts());
+    }
+
+    public function testIncrementingAttribute()
+    {
+        $category = new Category();
+        $this->assertFalse($category->incrementing);
+    }
+
+    public function testDatesAttribute()
+    {
+        $category = new Category();
+        $expected = ['deleted_at', 'created_at', 'updated_at'];
+        $actual = $category->getDates();
+        foreach ($expected as $current) {
+            $this->assertContains($current, $actual);
+        }
+        $this->assertCount(count($expected), $actual);
     }
 }
